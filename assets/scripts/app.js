ac89f3595e4b85fab9c8ac08028cca053d113139
akashmondal2000 <mondalakash494@gmail.com>
2024-04-01 02:42:33 +0530

    create bonus life

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,17 +3,25 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
-
-
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
+let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife)
 
 function endRound (){
+    const initialPlayerHelth = currentPlayerHealth;
     const playerDemege = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDemege;
+
+    if(currentPlayerHealth <= 0 && hasBonusLife){
+        hasBonusLife = false
+        removeBonusLife()
+        currentPlayerHealth = initialPlayerHelth;
+        setPlayerHealth(initialPlayerHelth)
+        alert("you would be dead but bonus life saved you")
+    }
 
     if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
         alert("You Won!")
