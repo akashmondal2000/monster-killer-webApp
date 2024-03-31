2b42cd6fcf1cd028941e1638c0600992bb86578c
akashmondal2000 <mondalakash494@gmail.com>
2024-04-01 03:23:51 +0530

    replace herd code String with constant

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,9 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
+const MODE_ATTACK =  "ATTACK" // MODE_ATTACK = 0
+const MODE_STRONG_ATTACK = "STRONG_ATTACK" // MODE_STRONG_ATTACK = 1
+
 const enteredInput = prompt("Enter maximum life for you and monster.,'100' ");
 
 let chosenMaxLife = parseInt(enteredInput);
@@ -50,9 +53,9 @@ function endRound() {
 
 function attactMonster(mode) {
   let maxDamege;
-  if (mode === "ATTACK") {
+  if (mode === MODE_ATTACK) {
     maxDamege = ATTACK_VALUE;
-  } else if (mode === "STRONG_ATTACK") {
+  } else if (mode === MODE_STRONG_ATTACK) {
     maxDamege = STRONG_ATTACK_VALUE;
   }
 
@@ -63,11 +66,11 @@ function attactMonster(mode) {
 }
 
 function attackHandler() {
-  attactMonster("ATTACK");
+  attactMonster(MODE_ATTACK);
 }
 
 function strongAttackHandler() {
-  attactMonster("STRONG_ATTACK");
+  attactMonster(MODE_STRONG_ATTACK);
 }
 
 function healPlayerHandler() {
