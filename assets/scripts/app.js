71029ae534e76c6201531823f190dc8d683a55d2
akashmondal2000 <mondalakash494@gmail.com>
2024-03-31 13:39:03 +0530

    damage player health

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
 const ATTACT_VALUE = 10;
+const MONSTER_ATTACT_VALUE = 11;
+
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
@@ -9,8 +11,16 @@ function attackHandler(){
     
     const damage = dealMonsterDamage(ATTACT_VALUE);
     currentMonsterHealth -= damage; // currentMonsterHealth = currentMonsterHealth - damage //both are same -> 1st one is short hand//
-    if(currentMonsterHealth <= 0){
+    
+    const playerDemege = dealPlayerDamage(MONSTER_ATTACT_VALUE);
+    currentPlayerHealth -= playerDemege;
+
+    if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
         alert("You Won!")
+    }else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
+        alert("You Lost!")
+    }else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0 ){
+        alert("You draw this game")
     }
 
 }
