80650967118cb9f7d69aa8a5c7fdd4925ea7c4bc
akashmondal2000 <mondalakash494@gmail.com>
2024-03-31 15:24:46 +0530

    reduce code with monster attack functin

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,6 @@
-const ATTACT_VALUE = 10;
-const STRONG_ATTACT_VALUE = 20;
-const MONSTER_ATTACT_VALUE = 11;
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 20;
+const MONSTER_ATTACK_VALUE = 14;
 
 
 
@@ -10,12 +10,19 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife)
 
-function attackHandler(){
-    
-    const damage = dealMonsterDamage(ATTACT_VALUE);
+
+function attactMonster (mode){
+    let maxDamege ;
+    if (mode === 'ATTACK'){
+        maxDamege = ATTACK_VALUE
+    }else if (mode === 'STRONG_ATTACK'){
+        maxDamege = STRONG_ATTACK_VALUE
+    }
+
+    const damage = dealMonsterDamage(maxDamege);
     currentMonsterHealth -= damage; // currentMonsterHealth = currentMonsterHealth - damage //both are same -> 1st one is short hand//
     
-    const playerDemege = dealPlayerDamage(MONSTER_ATTACT_VALUE);
+    const playerDemege = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDemege;
 
     if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
@@ -25,24 +32,15 @@ function attackHandler(){
     }else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0 ){
         alert("You draw this game")
     }
+}
+
+function attackHandler(){
+    attactMonster("ATTACK")
 
 }
 
 function strongAttackHandler (){
-
-    const damage = dealMonsterDamage(STRONG_ATTACT_VALUE);
-    currentMonsterHealth -= damage; // currentMonsterHealth = currentMonsterHealth - damage //both are same -> 1st one is short hand//
-    
-    const playerDemege = dealPlayerDamage(MONSTER_ATTACT_VALUE);
-    currentPlayerHealth -= playerDemege;
-
-    if(currentMonsterHealth <= 0 && currentPlayerHealth > 0){
-        alert("You Won!")
-    }else if(currentPlayerHealth <= 0 && currentMonsterHealth > 0){
-        alert("You Lost!")
-    }else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0 ){
-        alert("You draw this game")
-    }
+    attactMonster("STRONG_ATTACK")
 }
 
 attackBtn.addEventListener('click',attackHandler)
